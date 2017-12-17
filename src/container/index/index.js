047808a724b3f536381ab3abb1753a6326f974ee
ClanceyLu <ClanceyLu@gmail.com>
2017-12-17 11:15:18 +0800

    使用less

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -3,18 +3,19 @@ import React from 'react'
 import Header from '../../component/header/header'
 import Footer from '../../component/footer/footer'
 import Article from '../../component/article/article'
-import Painal from '../../component/painal/painal'
+import Info from '../../component/info/info'
+import style from './index.less'
 class Index extends React.Component {
   render() {
     return (
-      <div className='flex'>
+      <div className={style.flex}>
         <Header />
-        <div id='content'>
+        <div className={style.content}>
           <main>
             <Article title='齐物论' author='南华真人' />
           </main>
           <aside>
-            <Painal />
+            <Info />
           </aside>
         </div>
         <Footer />
