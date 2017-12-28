935196508ccb4f626101077e5b479e282d474d91
ClanceyLu <ClanceyLu@gmail.com>
2017-12-28 10:27:31 +0800

    增加路由

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
 import style from './App.less'
 import Index from './container/index/index'
+import About from './container/about/about'
+
+import Header from './component/header/header'
+import Footer from './component/footer/footer'
 
 class App extends Component {
   render() {
     return (
       <div className={style.App}>
-        <Index />
+        <Router>
+          <div>
+            <Header />
+            <Route exact path='/' component={Index} />
+            <Route path='/about' component={About} />
+            <Footer />
+          </div>
+        </Router>
       </div>
     )
   }
--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import style from './header.less'
 class Header extends React.Component {
@@ -10,7 +11,8 @@ class Header extends React.Component {
           <nav>
             <ul>
               <li>
-                <a className={style.active} href="index.html">首页</a>
+                <Link className={style.active} to='/'>首页</Link>
+                {/* <a className={style.active} href="index.html">首页</a> */}
               </li>
               <li>
                 <a href="">学海无涯</a>
@@ -20,7 +22,7 @@ class Header extends React.Component {
               </li>
 
               <li>
-                <a href="about.html">关于</a>
+                <Link to='/about'>关于</Link>
               </li>
             </ul>
           </nav>
