3da8cc66780add4bc3e6adf554423dcaf0c99ef0
ClanceyLu <ClanceyLu@gmail.com>
2018-01-02 17:01:23 +0800

    添加手风琴导航

--- src/admin.js
+++ src/admin.js
@@ -2,17 +2,55 @@ import React from 'react'
 import {
   BrowserRouter as Router,
   Route,
+  Link,
   Redirect
 } from 'react-router-dom'
 
-import Login from './container/admin/login/login'
+import { getCookie } from './util'
+
+import style from './admin.less'
 
 class Admin extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userid: getCookie('userid'),
+      test: true
+    }
+    this.show = this.show.bind(this)
+  }
+  show(t) {
+    this.setState({
+      test: t
+    })
+  }
   render() {
     return (
       <Router>
-        <div style={{height: '100%'}}>
-          <Route exact component={Login} />
+        <div className={style.admin}>
+          {this.state.userid ? null : this.props.history.push('/admin/login')}
+          <aside>
+            <h2 className={style.logo}>Clancey</h2>
+            <ul>
+              <li className={this.state.test === 'list' ? style.hide : null}>
+                <div onClick={() => this.show('list')}>文章列表</div>
+                <ul>
+                  <li>文章分类</li>
+                  <li>文章查询</li>
+                </ul>
+              </li>
+              <li className={this.state.test === 'edit' ? style.hide : null}>
+                <div onClick={() => this.show('edit')}>文章编辑</div>
+                <ul>
+                  <li>添加文章</li>
+                  <li>修改文章</li>
+                </ul>
+              </li>
+            </ul>
+          </aside>
+          <main>
+            mian
+          </main>
         </div>
       </Router>
     )
